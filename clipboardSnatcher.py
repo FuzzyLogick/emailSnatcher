8a84d6d514dbea9bae7082d6200e4a91f17688be
VigoTheCarpathian <ienjoythe_nightlife@hotmail.com>
2017-11-14 05:42:24 -0500

    updated verbage.  process remains the same

--- clipboardSnatcher.py
+++ clipboardSnatcher.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python3
-'''Little Python script that pulls email addresses from clipboard.
-pyperclip is a dependency, so you will need to install it via pip.
+'''
+ ██████╗██╗     ██╗██████╗ ██████╗  ██████╗  █████╗ ██████╗ ██████╗                            
+██╔════╝██║     ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔══██╗                           
+██║     ██║     ██║██████╔╝██████╔╝██║   ██║███████║██████╔╝██║  ██║                           
+██║     ██║     ██║██╔═══╝ ██╔══██╗██║   ██║██╔══██║██╔══██╗██║  ██║                           
+╚██████╗███████╗██║██║     ██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝                           
+ ╚═════╝╚══════╝╚═╝╚═╝     ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝                            
+                                                                                               
+        ███████╗███╗   ██╗ █████╗ ████████╗ ██████╗██╗  ██╗███████╗██████╗    ██████╗ ██╗   ██╗
+        ██╔════╝████╗  ██║██╔══██╗╚══██╔══╝██╔════╝██║  ██║██╔════╝██╔══██╗   ██╔══██╗╚██╗ ██╔╝
+        ███████╗██╔██╗ ██║███████║   ██║   ██║     ███████║█████╗  ██████╔╝   ██████╔╝ ╚████╔╝ 
+        ╚════██║██║╚██╗██║██╔══██║   ██║   ██║     ██╔══██║██╔══╝  ██╔══██╗   ██╔═══╝   ╚██╔╝  
+        ███████║██║ ╚████║██║  ██║   ██║   ╚██████╗██║  ██║███████╗██║  ██║██╗██║        ██║   
+        ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝  
+
+Little Python script that pulls email addresses from the clipboard.
+
 Warning.  If you are using Debian based OS, you may get this error:
 "pyperclip could not find a copy/paste mechanism for your system."
-If you do, make sure you install xsel (sudo apt install xsel) and 
-this should fix it.
+If you do, make sure you install xsel (sudo apt install xsel) and this should fix it.
 
-TO USE - It doesn't matter what the source of the data is (doc, txt, 
-pdf, xls, web page, whatever). Just Ctrl + A and then Ctrl + C that 
-bad boy... the entire thing.  A copy of everything will be held 
-in memory, on the clipboard. Next, run clipboardSnatcher to extract 
-emails from the clipboard.  No need to do anything else, just enter 
-./clipboardSnatcher.py and it will pull emails from the clipboard 
-and output them to the terminal.  But wait, there's more.  It will 
-also automatically put the snatched emails back onto the clipboard 
-for you to paste them into a text file for later.  Based on the 
-work by Al Sweigart.
+TO USE - It doesn't matter what the source of the data is (doc, txt, pdf, xls, web page, whatever).
+just Ctrl + A and then Ctrl + C that bad boy... the entire thing.  A copy of everything will be held on the clipboard. Next, run clipboardSnatcher.py to extract emails from the clipboard.  No need to do anything else, just enter ./clipboardSnatcher.py and it will pull emails from the clipboard and output them to the terminal.  But wait, there's more.  It will also automatically put the snatched emails back onto the clipboard for you to paste into a text file.  Based on the work by Al Sweigart (he does good work).
 
-Why use? - This tool isn't dependant on the what type of format the 
-data is stored in.  For example, you could run across the online employee 
-directory for company MegaCorpOne.  You could simply Ctrl + A and 
-Ctrl + C the entire directory and run clipboardSnatcher to extract 
-the emails.  This tool is perfect for those times when you have access 
-to data, but for one reason or another, don't have the ability to access 
-the data directly as a file to point this script to.  If you can highlight 
-and copy the entire thing, you're golden.
+Why use? - This tool isn't dependant on the what type of format the data is stored in.  For example, you could run across the online employee directory for company MegaCorpOne.  You could simply Ctrl + A and Ctrl + C the entire directory and run clipboardSnatcher to extract the emails.  This tool is perfect for those times when you have access to data, but for one reason or another, don't have the ability to access the data directly as a file.  If you can highlight and copy the entire thing, you're golden.
+
+This script depends on Pyperclip to run.
+
+This Python script was written by James Russell.
+
+Usage:  ./clipboardSnatcher.py
+
 '''
 
 
 import pyperclip, re
+
 
 #Create email regex.
 emailRegex = re.compile(r'''(
@@ -48,7 +55,8 @@ for groups in emailRegex.findall(text):
 #Copy results to the clipboard.
 if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
-    print('Copied to clipboard:')
+    print("Emails found!  Ready to paste!")
+    print("")
     print('\n'.join(matches))
 else:
-    print('No email addresses found.')
+    print("No email addresses found.")
